117243acfed41438553f1392b0b709107c6f3d21
DeveloperSky95 <haneulgim95@gmail.com>
2026-04-27 17:16:54 +0900

    중첩 라우팅 구현- practice

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Outlet } from "react-router";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+
+const Wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100dvh;
+`;
+
+const Main = styled.main`
+    flex: 1;
+`;
+
+function MainLayout() {
+    return (
+        <Wrap>
+            <Header></Header>
+            <Main>
+                <Outlet />
+            </Main>
+            <Footer></Footer>
+        </Wrap>
+    );
+}
+
+export default MainLayout;
---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+function About() {
+    return <>
+        <h1>About</h1>
+        <p>심플한 어바웃 화면입니다.</p>
+    </>
+}
+
+export default About;
